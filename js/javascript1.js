b8a29d8685c75c633e61e066d74cff0d5302e518
Martin Wagner <32431367+MartinWagner88@users.noreply.github.com>
2017-11-05 21:46:38 +0100

    AUslagern der Tabs in "Patient" als iFrame-Dokumente

---
+++ js/javascript1.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#testbutton").click(function(){
+    $("#test").hide();
+  });
+
+  //Tabelle mit Patientennamen einblenben, wenn SuchSubmit-Button gedrückt wird
+  $("#pattabelle").click(function(){
+    $("#pattabelle").hide();
+  });
+  $("#verlaufbtn").click(function(){
+    $("#hide").hide();
+  })
+  });
+
+});
