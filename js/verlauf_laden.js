6ba21eb7019366b5dde6643db730c501782b21b7
MarcelVetter <32433031+MarcelVetter@users.noreply.github.com>
2018-03-17 22:01:41 +0100

    verlaufUebersich_laeuft

--- js/verlauf_laden.js
+++ js/verlauf_laden.js
@@ -1,8 +1,38 @@
-function verlauf_laden(selectedPatientID){
-        alert(selectedPatientID);
-         $.ajax({
-              type: 'POST',
-              url: 'verlauf_gesamt.php',
-              data: {patient: selectedPatientID}
-          });
-}
+	function verlauf_laden_funktion(selPatID){
+          var PatIDTrans='patient='+selPatID
+          var xhttp;
+          xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200){
+              if (!this.response){
+                $("#patTableDiv").css("display","none");
+                $("#noPatientError").css("display","inline");
+              } else {
+              //alert(this.response);
+              document.getElementById('verlauf_container').innerHTML = this.responseText;
+              }
+            }
+          };
+          xhttp.open("POST","verlauf_gesamt_db.php",true);
+          xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          xhttp.send(PatIDTrans);
+	};
+
+/*
+var jahre = [result.length];
+var monate = [result.length];
+var tage = [result.length];
+var stunden = [result.length];
+var minuten = [result.length];
+var kommentare = [result.length];
+var arzt = [result.length];
+
+for(i=0; i<result.length;i++){
+  jahre[i]=result[i][0];
+  monate[i]=result[i][1];
+  tage[i]=result[i][2];
+  stunden[i]=result[i][3];
+  minuten[i]=result[i][4];
+  kommentare[i]=result[i][5];
+  arzt[i]=result[i][6];
+*/
---
+++ verlauf_gesamt_db.php
@@ -0,0 +1,43 @@
+<?php
+$id_stammdaten = $_POST['patient'];
+
+include_once("db_connect.php");
+$pdo = new PDO($pdoSerAUs, $username, $password);
+
+$sql = "SELECT datum, kommentar, arzt FROM bericht WHERE stammdaten_idstammdaten='$id_stammdaten' ORDER BY datum DESC";
+
+$ErgArrayPos=0;
+$verlaufErgArray=array();
+
+$verlaufErg = $pdo->query($sql);
+
+foreach ($verlaufErg as $row) {
+  //$ErgArrayPos_2=0;
+	$datum=strtok($row['datum'], " ");
+	$uhrzeit=strtok("");
+	$jahr=strtok($datum,"-");
+	$monat=strtok("-");
+	$tag=strtok("-");
+	$stunden=strtok($uhrzeit, ":");
+	$minuten=strtok(":");
+
+  $verlaufErgArray[$ErgArrayPos]= "<div class='panel panel-default'>
+	       <div class='panel-heading'>
+	        <h3 class='panel-title'>
+	         <a data-toggle='collapse' data-parent='#accordion' href='#collapse".$ErgArrayPos."'>".$tag.".".$monat.".".$jahr.", ".$stunden.":".$minuten." &ndash; ".$row['arzt']."</a></h3></div>
+	         <div id='collapse".$ErgArrayPos."' class='panel-collapse collapse";
+  if ($ErgArrayPos==0){$verlaufErgArray[$ErgArrayPos].="in";};
+  $verlaufErgArray[$ErgArrayPos].="'><div class='panel-body'>".$row['kommentar']."</div></div></div>";
+
+  $ErgArrayPos++;
+}
+
+$ErgFinal="<div class='container'><div class='panel-group' id='accordion'> ";
+
+for ($i=0; $i<count($verlaufErgArray);$i++){
+  $ErgFinal.=$verlaufErgArray[$i];
+}
+
+echo ($ErgFinal);
+//echo json_encode($verlaufErgArray);
+?>
